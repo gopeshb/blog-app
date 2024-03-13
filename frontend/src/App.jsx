3c46dec9680ef683a45a9b3320f84fc6e16133e9
gopeshb <gopeshbhatia01@outlook.com>
2024-03-13 22:43:18 +0530

    routes created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SIgnIn'
+import Dashboard from './pages/DashBoard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
 function App() {
 
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
